9a3ba42c4422e047003cfc1b2e977cffba30aeac
Fitsyu Two <fitsyu2@gmail.com>
2020-05-01 05:59:08 +0700

    Create session manager to save paused session

---
+++ Dzikra WatchKit Extension/Data Layer/SessionManager.swift
@@ -0,0 +1,46 @@
+//
+//  SessionManager.swift
+//  Dzikra WatchKit Extension
+//
+//  Created by Fitsyu  on 01/05/20.
+//  Copyright © 2020 Fitsyu . All rights reserved.
+//
+
+import Foundation
+
+protocol SessionManager {
+    func save(session: DzikrSession)
+    func restore(completion: @escaping (DzikrSession) -> Void)
+}
+
+class UserDefaultsSessionManager: SessionManager {
+    
+    let storage = UserDefaults.standard
+    let key = "pausedSession"
+    
+    func save(session: DzikrSession) {
+        
+        do {
+            let data = try JSONEncoder().encode(session)
+            storage.set(data, forKey: key)
+            
+        } catch {
+            print(error)
+        }
+    }
+    
+    func restore(completion: @escaping (DzikrSession) -> Void) {
+        
+        do {
+            
+            if let data = storage.value(forKey: key) as? Data {
+                
+                let session = try JSONDecoder().decode(DzikrSession.self, from: data)
+                completion(session)
+            }
+            
+        } catch {
+            print(error)
+        }
+    }
+}
